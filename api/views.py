b02c49f9b454479353503f6aa91fd1ae58d9da07
mericalp <meric.winn.alp@gmail.com>
2024-02-06 16:03:31 +0300

    serializers notes

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
 # Create your views here.
 
@@ -40,6 +42,21 @@ def getRoutes(request):
     ]
     
     return Response(routes)
+
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
+
 
 
 
