06fd1f6d823c66d016750d128ecfecc1b175b89d
mericalp <meric.winn.alp@gmail.com>
2024-02-07 19:55:28 +0300

    deleted src

---
+++ client/src/pages/NotesListPage.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react'
+
+const NotesListPage = () => {
+
+  let [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    getNotes()
+  }, [])
+
+  let getNotes = async  () => {
+    
+    let response = await fetch('http://127.0.0.1:8000/api/notes/')
+    let data = await response.json()
+    console.log('Data:', data)
+    setNotes(data)
+
+  }
+  
+  return (
+    <div>
+        notes
+    </div>
+  )
+}
+
+export default NotesListPage
